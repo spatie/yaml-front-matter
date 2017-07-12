9ce3bfe7f4982898705c47a7ba74182db54c2952
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-07-12 09:23:10 +0200

    Add parseFile

--- src/Parser.php
+++ src/Parser.php
@@ -2,12 +2,11 @@
 
 namespace Spatie\YamlFrontMatter;
 
-use Exception;
 use Symfony\Component\Yaml\Yaml;
 
 class Parser
 {
-
+    /** @var \Symfony\Component\Yaml\Yaml */
     protected $yamlParser;
 
     public function __construct()
@@ -15,7 +14,7 @@ class Parser
         $this->yamlParser = new Yaml();
     }
 
-    public function parse(string $content) : Document
+    public function parse(string $content): Document
     {
         $pattern = '/[\s\r\n]---[\s\r\n]/s';
 
@@ -24,9 +23,18 @@ class Parser
         if (count($parts) < 3) {
             return new Document([], $content);
         }
+
         $matter = $this->yamlParser->parse(trim($parts[1]));
+
         $body = implode(PHP_EOL . "---" . PHP_EOL, array_slice($parts, 2));
+
         return new Document($matter, $body);
     }
 
+    public function parseFile(string $path): Document
+    {
+        return $this->parse(
+            file_get_contents($path)
+        );
+    }
 }
