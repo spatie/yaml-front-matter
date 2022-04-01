93f454564d95ec8e430f3a374a571e525e409e86
Caen De Silva <caen@desilva.se>
2022-04-01 12:01:48 +0200

    Add more tests

--- tests/YamlFrontMatterMarkdownCompatibleParseTest.php
+++ tests/YamlFrontMatterMarkdownCompatibleParseTest.php
@@ -31,4 +31,46 @@ class YamlFrontMatterMarkdownCompatibleParseTest extends TestCase
         $this->assertEquals(['foo' => 'bar'], $document->matter());
         $this->assertStringContainsString('Lorem ipsum.', $document->body());
     }
+    
+    /** @test */
+    public function it_separates_the_front_matter_from_the_body()
+    {
+        $document = YamlFrontMatter::markdownCompatibleParse(
+           "---\ntitle: Front Matter\n---\n\nLorem ipsum."
+        );
+
+        $this->assertInstanceOf(Document::class, $document);
+
+        // This implicitly asserts that the front matter does not contain any markdown
+        $this->assertEquals(['title' => 'Front Matter'], $document->matter());
+        // This implicitly asserts that the body does not contain any front matter remnants
+        $this->assertEquals('Lorem ipsum.', $document->body());
+    }
+       
+    /** @test */
+    public function it_leaves_string_without_front_matter_intact()
+    {
+        $document = YamlFrontMatter::markdownCompatibleParse(
+            "Lorem ipsum."
+        ); 
+
+        $this->assertInstanceOf(Document::class, $document);
+        $this->assertEmpty($document->matter());
+        $this->assertEquals('Lorem ipsum.', $document->body());
+    }
+
+    /** @test */
+    public function it_can_parse_a_file_partial_front_matter()
+    {
+        // If there is only one YAML control block, (---) the front matter is invalid
+        // and the document should be interpreted as having no front matter.
+
+        $document = YamlFrontMatter::markdownCompatibleParse(
+           "---\ntitle: Front Matter\n\nLorem ipsum."
+        );
+
+        $this->assertInstanceOf(Document::class, $document);
+        $this->assertEmpty($document->matter());
+        $this->assertEquals("---\ntitle: Front Matter\n\nLorem ipsum.", $document->body());
+    }
 }
